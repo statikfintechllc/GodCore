a7613207a417f60c3d8c27876f07677defe9ef5d
statikfintechllc <ascend.gremlin@gmail.com>
2025-06-10 21:09:54 -0500

    i sumbited a bug, now im unsubmitting it😅🤷‍♂️🤦‍♂️

---
+++ backend/stop_all.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+cd "$(dirname "$0")/.."       # Always go to repo root from scripts/
+export PYTHONPATH="$PWD"
+
+pkill -f react-scripts
+pkill -f node        
+pkill -f run_llama.py
+lsof -ti:3000 | xargs -r kill -9
+lsof -ti:8000 | xargs -r kill -9
+pkill -f ngrok
+for port in 8000 3000 7070 9973; do
+  lsof -ti :$port | xargs -r kill -9
+done
